f875e2acd3bfaa4ff6dd5467c894ab4567245f66
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-03-10 12:23:31 -0400

    .

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -1,13 +1,14 @@
 { pkgs, ... }:
 
 {
-  # List packages installed in system profile. To search, run:
-  # $ nix search vim
   environment.systemPackages = with pkgs; [
     vim
+
     wget
     curl
+
     tree
     htop
+    unzip
   ];
 }
