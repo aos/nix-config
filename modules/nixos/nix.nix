784bf353a33d16ac5a4cd59ce10f5ec7867e1a77
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-10-01 18:36:36 -0400

    .

--- modules/nixos/nix.nix
+++ modules/nixos/nix.nix
@@ -32,12 +32,14 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      # Keep the last 5 generations
-      options = "--delete-older-than +5";
-    };
+    # TODO: can't really use this as the +5 modifier doesn't work...
+    # See: https://github.com/NixOS/nix/pull/9894
+    # gc = {
+    #   automatic = true;
+    #   dates = "weekly";
+    #   # Keep the last 5 generations
+    #   options = "--delete-older-than +5";
+    # };
 
     # https://discourse.nixos.org/t/difference-between-nix-settings-auto-optimise-store-and-nix-optimise-automatic/25350
     optimise.automatic = true;
