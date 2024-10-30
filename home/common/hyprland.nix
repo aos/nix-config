f584ff6f85c5d7d93df40f4efed1e8c5a998abf9
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-10-30 08:46:18 -0400

    .

--- home/common/hyprland.nix
+++ home/common/hyprland.nix
@@ -1,13 +1,23 @@
 { home, ... }:
 
 {
-  home.file = {
-    ".config/hypr/".source = ./config/hypr;
-    ".config/swaylock/".source = ./config/swaylock;
-    ".config/swayidle/config".source = ./config/swayidle_config;
-    ".config/waybar/".source = ./config/waybar;
-    ".config/fuzzel/fuzzel.ini".source = ./config/fuzzel.ini;
-    ".config/mako/config".source = ./config/mako;
-    ".config/swappy/config".source = ./config/swappy;
+  home = {
+    sessionVariables = {
+      MOZ_ENABLE_WAYLAND = 1;
+      XDG_CURRENT_DESKTOP = "Hyprland";
+      XDG_SESSION_DESKTOP = "Hyprland";
+      XDG_SESSION_TYPE = "wayland";
+      GDK_BACKEND = "wayland,x11";
+    };
+
+    file = {
+      ".config/hypr/".source = ./config/hypr;
+      ".config/swaylock/".source = ./config/swaylock;
+      ".config/swayidle/config".source = ./config/swayidle_config;
+      ".config/waybar/".source = ./config/waybar;
+      ".config/fuzzel/fuzzel.ini".source = ./config/fuzzel.ini;
+      ".config/mako/config".source = ./config/mako;
+      ".config/swappy/config".source = ./config/swappy;
+    };
   };
 }
--- modules/nixos/userland.nix
+++ modules/nixos/userland.nix
@@ -20,7 +20,6 @@
     waybar
     libnotify
 
-    # swww      # Wallpaper daemon
     mako # notification daemon
     fuzzel # Launcher
     # raffi # fuzzel dmenu launcher
@@ -34,11 +33,6 @@
     brightnessctl
 
     wl-gammarelay-rs
-    # busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n -500
-    # busctl --user -- call rs.wl-gammarelay / rs.wl.gammarelay UpdateTemperature n +500
-    # busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 4000
-    # busctl --user -- set-property rs.wl-gammarelay / rs.wl.gammarelay Temperature q 6500
-
     grim # screenshot
     slurp # select region -> grim "$(slurp)" - | wl-copy
     swappy # grim -g "$(slurp)" - | swappy -f -
@@ -47,6 +41,8 @@
     imv # image viewer
     qalculate-gtk # calculator
     nautilus # file viewer
+    wl-clipboard
+    xwaylandvideobridge # screensharing bridge
 
     qt5.qtwayland
     qt6.qtwayland
@@ -63,16 +59,26 @@
 
   xdg.portal = {
     enable = true;
+    config = {
+      common = {
+        default = [ "hyprland" ];
+      };
+      hyprland = {
+        default = [ "gtk" "hyprland" ];
+      };
+    };
     extraPortals = [
       pkgs.xdg-desktop-portal-gtk
       inputs.hyprland.packages.${pkgs.system}.xdg-desktop-portal-hyprland
     ];
+    xdgOpenUsePortal = true;
   };
 
   fonts.packages = with pkgs; [
     cantarell-fonts
     font-awesome
     noto-fonts-emoji
+    material-design-icons
     (nerdfonts.override {
       fonts = [
         "Inconsolata"
