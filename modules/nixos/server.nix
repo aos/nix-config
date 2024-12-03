1a2279320071ab65d543079ddf3461a83bcb7cf5
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2024-12-02 19:13:39 -0500

    .

--- modules/nixos/server.nix
+++ modules/nixos/server.nix
@@ -6,10 +6,6 @@
     inputs.srvos.nixosModules.server
   ];
 
-  # Until this goes in: https://git.clan.lol/clan/clan-core/pulls/2528
-  systemd.network.networks."99-ethernet-default-dhcp".networkConfig.MulticastDNS = lib.mkForce true;
-  systemd.network.networks."99-wireless-client-dhcp".networkConfig.MulticastDNS = lib.mkForce true;
-
   users.users.root.openssh.authorizedKeys.keyFiles = [ ../../sops/keys/aos/authorized_keys ];
   users.users.mei = {
     isNormalUser = true;
