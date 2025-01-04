4df3fa3dddb77b7c1deb73d66b0db17171151707
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2025-01-04 12:12:20 -0500

    .

--- home/aos/config/default.nix
+++ home/aos/config/default.nix
@@ -14,6 +14,7 @@
 
   home.file.".tmux.conf".source = ./tmux;
 
+  home.file.".config/jj/config.toml".source = ./jj_config.toml;
   home.file.".gitconfig".source = ./gitconfig;
   home.file.".ignore".source = ./rg_ignore;
   home.file.".config/foot/foot.ini".source = ./terminal/foot.ini;
--- modules/nixos/monitoring.nix
+++ modules/nixos/monitoring.nix
@@ -5,5 +5,7 @@
     enable = true;
     openFirewall = true;
     enabledCollectors = [ "systemd" ];
+    # --collector.filesystem.mount-points-exclude=^/(dev|proc|sys|var/lib/docker/.+|var/lib/kubelet/.+)($|/)
+    # --collector.filesystem.fs-types-exclude=^(autofs|binfmt_misc|bpf|cgroup2?|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|iso9660|mqueue|nsfs|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|selinuxfs|squashfs|sysfs|tracefs)$
   };
 }
