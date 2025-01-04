bb1d4fbbd286c818a58a46c0e5ef9b2fb8cd6da1
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2025-01-04 15:49:48 -0500

    .

--- modules/nixos/monitoring.nix
+++ modules/nixos/monitoring.nix
@@ -4,8 +4,10 @@
   services.prometheus.exporters.node = {
     enable = true;
     openFirewall = true;
-    enabledCollectors = [ "systemd" ];
-    # --collector.filesystem.mount-points-exclude=^/(dev|proc|sys|var/lib/docker/.+|var/lib/kubelet/.+)($|/)
-    # --collector.filesystem.fs-types-exclude=^(autofs|binfmt_misc|bpf|cgroup2?|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|iso9660|mqueue|nsfs|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|selinuxfs|squashfs|sysfs|tracefs)$
+    enabledCollectors = [
+      "systemd"
+      "filesystem.mount-points-exclude=^/(dev|proc|sys|var/lib/docker/.+|var/lib/kubelet/.+)($|/)"
+      "filesystem.fs-types-exclude=^(autofs|binfmt_misc|bpf|cgroup2?|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|iso9660|mqueue|nsfs|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|selinuxfs|squashfs|sysfs|tracefs)$"
+    ];
   };
 }
