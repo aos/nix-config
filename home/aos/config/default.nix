55b667e8aa577a5af1faed9ce30d864a47ca7a7a
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2025-01-13 20:57:53 -0500

    .

--- home/aos/config/default.nix
+++ home/aos/config/default.nix
@@ -15,11 +15,37 @@
       };
       aliases = {
         d = ["diff"];
+        l = ["log"];
       };
       ui = {
         pager = "delta";
-        default-command = ["log"];
+        default-command = ["log" "-T" "builtin_log_oneline"];
+
         diff.format = "git";
+      };
+      template-aliases = {
+        "colored(color, txt)" = "surround(raw_escape_sequence('\\x01\\e[' ++ color ++ 'm\\x02'), raw_escape_sequence('\\x01\\e[0m\\x02'), txt)";
+        "red(txt)"     = "colored('31', txt)";
+        "yellow(txt)"  = "colored('33', txt)";
+        "magenta(txt)" = "colored('35', txt)";
+        "cyan(txt)"    = "colored('36', txt)";
+        "ps1_repo_info" = ''concat(
+          yellow("(@"),
+          surround(" ", " ", yellow(bookmarks)),
+          surround("", " -> ",
+            if(!description,  yellow(separate("+",
+              parents.map(|c| coalesce(
+              c.tags(), c.bookmarks(), c.change_id().shortest())
+              ))))),
+          if(empty,
+            magenta(change_id.shortest()),
+            magenta(change_id.shortest() ++ "*")),
+          surround(":", "", concat(
+            if(divergent, cyan("Y")),
+            if(conflict,  red("X")),
+          )),
+          yellow(")"),
+        )'';
       };
     };
   };
