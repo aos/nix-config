0653cbcfe876fdc8fc80af31af9c45b2d9396916
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2025-10-08 03:24:12 -0400

    .

--- hosts/tower/default.nix
+++ hosts/tower/default.nix
@@ -55,40 +55,41 @@ in
     };
   };
 
-  # Turn off some things that cause instant wakeup after suspend
-  services.udev.extraRules =
-    let
-      devs = [
-        # USB ports (xHCI)
-        # {
-        #   vendor = "0x8086";
-        #   device = "0xa0ed";
-        # }
-        # Thunderbolt 4 USB
-        {
-          vendor = "0x8086";
-          device = "0x9a13";
-        }
-      ];
-    in
-    lib.concatStringsSep "\n" (
-      map (devs: ''
-        # Turn off wakeup for some devices that causes poor suspend behavior
-        ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="${devs.vendor}" ATTR{device}=="${devs.device}" ATTR{power/wakeup}="disabled"
-      '') devs
-    ) + ''
-      # DFU (Internal bootloader for STM32 and AT32 MCUs)
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="2e3c", ATTRS{idProduct}=="df11", TAG+="uaccess"
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
+  services.udev = {
+    # https://community.frame.work/t/how-to-rebind-the-xf86rfkill-f10-media-key/62545/2
+    # sudo systemd-hwdb update && sudo udevadm trigger
+    extraHwdb = ''
+      # Rewrite RFKILL (airplane mode) of the `wireless device` to `prog1`.
+      evdev:input:b0018v32ACp0006*
+        KEYBOARD_KEY_100c6=prog1
     '';
-
-  # https://community.frame.work/t/how-to-rebind-the-xf86rfkill-f10-media-key/62545/2
-  # sudo systemd-hwdb update && sudo udevadm trigger
-  environment.etc."udev/hwdb.d/99-local.hwdb".text = ''
-    # Rewrite RFKILL (airplane mode) of the `wireless device` to `prog1`.
-    evdev:input:b0018v32ACp0006*
-      KEYBOARD_KEY_100c6=prog1
-  '';
+    # Turn off some things that cause instant wakeup after suspend
+    extraRules =
+      let
+        devs = [
+          # USB ports (xHCI)
+          # {
+          #   vendor = "0x8086";
+          #   device = "0xa0ed";
+          # }
+          # Thunderbolt 4 USB
+          {
+            vendor = "0x8086";
+            device = "0x9a13";
+          }
+        ];
+      in
+      lib.concatStringsSep "\n" (
+        map (devs: ''
+          # Turn off wakeup for some devices that causes poor suspend behavior
+          ACTION=="add" SUBSYSTEM=="pci" ATTR{vendor}=="${devs.vendor}" ATTR{device}=="${devs.device}" ATTR{power/wakeup}="disabled"
+        '') devs
+      ) + ''
+        # DFU (Internal bootloader for STM32 and AT32 MCUs)
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="2e3c", ATTRS{idProduct}=="df11", TAG+="uaccess"
+        SUBSYSTEM=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", TAG+="uaccess"
+      '';
+  };
 
   # Fix for broken mesa with Hyprland
   hardware.graphics = {
