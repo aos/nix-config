663718d786cf17ed49bab5a67a1521fcd7c68c92
Aos Dabbagh <25783780+aos@users.noreply.github.com>
2026-02-17 11:57:09 -0500

    .

--- home/aos/tao.nix
+++ home/aos/tao.nix
@@ -45,5 +45,7 @@
 
     (google-cloud-sdk.withExtraComponents ([ google-cloud-sdk.components.gke-gcloud-auth-plugin ]))
     awscli2
+
+    inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}.claude-code
   ];
 }
